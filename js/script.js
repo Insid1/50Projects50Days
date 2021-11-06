3a93b913f3d08da77ea0cf7ec5d7425fde823ba3
Oleg <insid1@mail.ru>
2021-11-06 22:12:03 +0700

    Small refactoring and work on second feature

--- js/script.js
+++ js/script.js
@@ -1,18 +1,70 @@
 'use strict'
 
-const images = document.querySelectorAll('.slider__inner-img');
+class ExpandingCards {
+    constructor(slider) {
+        this.slider = slider;
+    }
 
-function makeWider(imgCollection) {
-    imgCollection.forEach((img) => {
-        img.addEventListener('click', () => {
+    makeWider() {
+        this.slider.forEach((img) => {
+            img.addEventListener('click', () => {
+                this.slider.forEach((img) => {
+                    img.classList.remove('slider__active');
+                })
 
-            images.forEach((img) => {
-                img.classList.remove('slider__active');
+                img.classList.toggle('slider__active');
             })
-
-            img.classList.toggle('slider__active');
         })
-    })
+    }
+
+    activate() {
+        this.makeWider();
+    }
 }
 
-makeWider(images);
+let images = new ExpandingCards(document.querySelectorAll('.slider__inner-img'));
+
+images.activate();
+
+class ProgressSteps {
+    constructor(innerElements, buttons) {
+        this.innerElements = innerElements;
+        this.buttons = buttons;
+
+        this.firstElement = 1;
+        this.secondElement = 2;
+    }
+
+    moveRight() {
+        if (this.secondElement === this.innerElements.length) return;
+        this.firstElement += 1;
+        this.secondElement += 1;
+    }
+
+    moveLeft() {
+        if (this.firstElement === 1) return;
+        this.firstElement -= 1;
+        this.secondElement -= 1;
+    }
+
+    addIventsToButtons() {
+        this.buttons[0].addEventListener('click', () => {
+            this.moveLeft();
+        })
+
+        this.buttons[1].addEventListener('click', () => {
+            this.moveRight();
+        })
+    }
+
+
+
+
+}
+// const innerElements = document.querySelector('.progress-step').children;
+// let [firstElement, secondElement] = [1, 2];
+// innerElements[firstElement].classList.toggle('progress-step__active');
+// innerElements[secondElement].classList.toggle('progress-step__active');
+
+
+
