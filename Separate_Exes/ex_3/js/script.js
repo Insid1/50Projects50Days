851a520dfaa1bd541c9ffc267c9f42ebae013a38
Oleg <insid1@mail.ru>
2021-11-13 18:51:22 +0700

    refactoring js code

--- Separate_Exes/ex_3/js/script.js
+++ Separate_Exes/ex_3/js/script.js
@@ -1,23 +1,32 @@
 'use strict'
 
+class RotatingNavigationAnimation {
+    constructor(divArticle, menuButton, menuLinks) {
+        this.divArticle = divArticle;
+        this.menuButton = menuButton;
+        this.menuLinks = menuLinks;
+    }
+
+    assignToButton() {
+        this.menuButton.addEventListener('click', () => {
+            this.divArticle.classList.toggle('article__active');
+            this.menuButton.classList.toggle('corner-button__active');
+            this.menuLinks.classList.toggle('menu__active');
+        })
+    }
+
+    activate() {
+        this.assignToButton();
+    }
+}
+
+
 const article = document.querySelector('.article');
 const menuButton = document.querySelector('.corner-button');
 const menuLinks = document.querySelector('.menu');
 
-// article.classList.toggle('article__active');
-// menuButton.classList.toggle('corner-button__active');
-// menuButton.classList.toggle('menu__active');
-
-
-function assignToButton() {
-    menuButton.addEventListener('click', () => {
-        article.classList.toggle('article__active');
-        menuButton.classList.toggle('corner-button__active');
-        menuLinks.classList.toggle('menu__active');
-    })
-}
-
-assignToButton();
+const rotateMenu = new RotatingNavigationAnimation(article, menuButton, menuLinks);
+rotateMenu.activate()
 
 
 
